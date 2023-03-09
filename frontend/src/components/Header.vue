13b4d333e330a3dfeac43554005437b8b32ba275
Kamil <kjas92@gmail.com>
2023-03-09 17:14:35 +0100

    fix logo size

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -4,7 +4,7 @@
             <h1 class='font-main text-[3vw] text-center'>UwUstimates</h1>
             <h2 class='font-main text-sm underline text-center'>Cutest Agile Planning Poker</h2>
         </div>
-        <img class='w-40 h-auto align-middle mt-5' src="../assets/uwustimates-bg.png" alt="">
+        <img class='w-40 scale-75 h-auto align-middle mt-5' src="../assets/uwustimates-bg-mlg.png" alt="">
     </header>
 </template>
 <script>
