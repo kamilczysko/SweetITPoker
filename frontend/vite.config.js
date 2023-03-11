ecd7a05732521c4704ce5de1b1d292090838013a
Kamil <kjas92@gmail.com>
2023-03-11 20:08:31 +0100

    revert server setting

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,15 +6,15 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    proxy: {
-      '/': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  },
+  // server: {
+  //   proxy: {
+  //     '/': {
+  //       target: 'http://localhost:8080',
+  //       changeOrigin: true,
+  //       secure: false,
+  //     }
+  //   }
+  // },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
