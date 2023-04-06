068f7b911e8e9c4938cec89c922118ac7ee34319
Kamil <kjas92@gmail.com>
2023-04-06 20:44:44 +0200

    fix styles

--- frontend/src/components/ImageChoose.vue
+++ frontend/src/components/ImageChoose.vue
@@ -1,12 +1,17 @@
 <template>
     <div class='flex items-center justify-center bg-none'>
         <p class='text-left font-light'>{{ label }} : </p>
+
         <div class='flex justify-center w-3/5 align-middle cursor-pointer relative' v-on:click="toggle">
             <div class='rounded-full w-14 h-14 overflow-hidden hover:shadow-bg3 hover: shadow-md'>
                 <img v-show="!isOpen" class='scale-100 hover:scale-110 ' :src="getIconUrl(value)" alt="">
             </div>
-            <div v-show="isOpen" class='flex flex-wrap gap-1 w-[13vw] bg-bg2 rounded-xl p-1 bg-opacity-70 absolute' v-on:mouseleave="hide">
-                <div v-for=" img, index in images()" class='rounded-full w-11 h-11 overflow-hidden hover: shadow-md hover:shadow-bg3 focus:scale-xl' v-on:click="select(img, index)">
+
+            <div v-show="isOpen" class='flex flex-wrap gap-2 flex-shrink sm:w-[40vw] xl:w-full bg-bg2 rounded-xl p-1 bg-opacity-70 absolute'
+                v-on:mouseleave="hide">
+                <div v-for=" img, index in images()"
+                    class='rounded-full w-11 h-11 overflow-hidden hover: shadow-md hover:shadow-bg3 focus:scale-xl'
+                    v-on:click="select(img, index)">
                     <img class='scale-100 hover: shadow-xl hover:scale-110 focus:scale-120' :src="getIconUrl(img)">
                 </div>
             </div>
