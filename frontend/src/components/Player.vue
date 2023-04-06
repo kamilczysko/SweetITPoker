edfa4dc0c93f8f98aa9f8a737911c50a75dd19c2
Kamil <kjas92@gmail.com>
2023-04-06 22:10:52 +0200

    fix avatar size

--- frontend/src/components/Player.vue
+++ frontend/src/components/Player.vue
@@ -1,11 +1,11 @@
 <template>
     <div class='flex items-center gap-4 mt-3' >
-        <div class='rounded-full overflow-hidden w-16 h-16 ml-1' :class="{['shadow-round'] : getPlayer.selectedCard!=null}">
+        <div class='rounded-full overflow-hidden aspect-square sm:w-16 2xl:w-20 ml-1' :class="{['shadow-round'] : getPlayer.selectedCard!=null}">
             <img :src="getAvatar(getPlayer.avatarIdx)">
         </div>
         <div class='relative' :class="{ ['blur-[1px]']: getPlayer.isObserver }">
             <p class='text-xl'> {{ getPlayer.name }} </p>
-            <span class='font-secondary font-extralight text-[10px] absolute -top-2'>{{ getPlayer.role }}</span>
+            <span class='font-secondary text-[12px] absolute -top-3'>{{ getPlayer.role }}</span>
             <div class='p-1'>
                 <Checkbox label="Senpai (Admin)" :selected="getPlayer.isAdmin" @select="setAdmin"/>
                 <Checkbox label="Observer-chan" :selected="getPlayer.isObserver" @select="setObserver" />
