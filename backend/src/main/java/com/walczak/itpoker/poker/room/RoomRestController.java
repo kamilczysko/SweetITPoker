c0681dc3eb8c600b71445213e6c1b3bc113748ff
Kamil <kjas92@gmail.com>
2023-04-07 22:51:29 +0200

    Revert "add cors"
    This reverts commit 7284a07e5f43a90f7c6f0e070cce6f6f4b3285c9.

--- backend/src/main/java/com/walczak/itpoker/poker/room/RoomRestController.java
+++ backend/src/main/java/com/walczak/itpoker/poker/room/RoomRestController.java
@@ -12,7 +12,6 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/rest/room")
-@CrossOrigin(origins = "http://localhost:8080/")
 public class RoomRestController {
     
     private final RoomService roomService;
