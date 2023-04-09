fead2fecb841faa6a258433989b2ed0151a5aeb2
Kamil <kjas92@gmail.com>
2023-04-09 21:55:19 +0200

    fix property names

--- backend/src/main/java/com/walczak/itpoker/configuration/CaptchaSettings.java
+++ backend/src/main/java/com/walczak/itpoker/configuration/CaptchaSettings.java
@@ -4,7 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConfigurationProperties(prefix = "google.recaptcha.key")
+@ConfigurationProperties(prefix = "captcha")
 public class CaptchaSettings {
 
     private String secret;
