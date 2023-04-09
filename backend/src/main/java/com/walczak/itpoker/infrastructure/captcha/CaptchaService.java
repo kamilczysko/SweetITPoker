f84fed27d2d13670734b76bcd448fb13804b42bb
Kamil <kjas92@gmail.com>
2023-04-09 22:51:10 +0200

    refactor properties for captcha

--- backend/src/main/java/com/walczak/itpoker/infrastructure/captcha/CaptchaService.java
+++ backend/src/main/java/com/walczak/itpoker/infrastructure/captcha/CaptchaService.java
@@ -1,8 +1,7 @@
 package com.walczak.itpoker.infrastructure.captcha;
 
-import com.walczak.itpoker.configuration.CaptchaSettings;
 import com.walczak.itpoker.infrastructure.captcha.model.CaptchaResponse;
-import org.springframework.data.jpa.repository.query.JSqlParserUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.client.RestOperations;
@@ -12,38 +11,34 @@ import java.util.regex.Pattern;
 
 @Service
 public class CaptchaService {
-        private final CaptchaSettings captchaSettings;
-        private final RestOperations restTemplate;
+    @Value("${captcha.secret}")
+    private String secret;
+    private final RestOperations restTemplate;
 
-    public CaptchaService(CaptchaSettings captchaSettings, RestOperations restTemplate) {
-        this.captchaSettings = captchaSettings;
+    public CaptchaService(RestOperations restTemplate) {
         this.restTemplate = restTemplate;
     }
 
     private static Pattern RESPONSE_PATTERN = Pattern.compile("[A-Za-z0-9_-]+");
 
-        public void processResponse(String response, String ip) {
-            if(!responseSanityCheck(response)) {
-                throw new IllegalStateException("Response contains invalid characters");
-            }
-
-            System.out.println("#######################");
-            System.out.println(captchaSettings.getSecret());
-            System.out.println("#######################");
-
-            URI verifyUri = URI.create(String.format(
-                    "https://www.google.com/recaptcha/api/siteverify?secret=%s&response=%s&remoteip=%s",
-                    captchaSettings.getSecret(), response, ip));
-
-            CaptchaResponse googleResponse = restTemplate.getForObject(verifyUri, CaptchaResponse.class);
-
-            if(!googleResponse.success()) {
-                throw new IllegalStateException("reCaptcha was not successfully validated");
-            }
+    public void processResponse(String response, String ip) {
+        if (!responseSanityCheck(response)) {
+            throw new IllegalStateException("Response contains invalid characters");
         }
 
-        private boolean responseSanityCheck(String response) {
-            return StringUtils.hasLength(response) && RESPONSE_PATTERN.matcher(response).matches();
+        URI verifyUri = URI.create(String.format(
+                "https://www.google.com/recaptcha/api/siteverify?secret=%s&response=%s&remoteip=%s",
+                secret, response, ip));
+
+        CaptchaResponse googleResponse = restTemplate.getForObject(verifyUri, CaptchaResponse.class);
+
+        if (!googleResponse.success()) {
+            throw new IllegalStateException("reCaptcha was not successfully validated");
+        }
+    }
+
+    private boolean responseSanityCheck(String response) {
+        return StringUtils.hasLength(response) && RESPONSE_PATTERN.matcher(response).matches();
 
     }
 }
