dc20ad9b5153a8e688fe22ef5a46e9e07b9705e8
Kamil <kjas92@gmail.com>
2023-04-18 18:28:20 +0200

    add new adds, remove old ads, fix header size

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -2,7 +2,7 @@
     <header class='flex flex-col items-center justify-center mt-5'>
         <div>
             <h1 class='font-main text-[2vw] text-center'>UwUstimates</h1>
-            <h2 class='font-main text-sm underline text-center'>Cutest Agile Planning Poker</h2>
+            <h2 class='font-main text-lg underline text-center'>Cutest Agile Planning Poker</h2>
         </div>
         <img class='w-40 scale-75 h-auto align-middle mt-5' src="../assets/uwustimates-bg-mlg.png" alt="">
     </header>
