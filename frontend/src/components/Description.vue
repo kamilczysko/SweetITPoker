0d1826a5feb40fd09187db85f6817ded598a2651
Kamil <kjas92@gmail.com>
2023-05-07 17:28:22 +0200

    add rockets

--- frontend/src/components/Description.vue
+++ frontend/src/components/Description.vue
@@ -1,21 +1,31 @@
 <template>
-        <article class='mx-5'>
-            <div class='flex flex-col justify-center items-center gap-5'>
-                <h2 class='xs:text-lg lg:text-2xl'>Your perfect planning tool</h2>
-                <p class='font-secondary xs:text-sm lg:text-md font-light text-center'>
-                    <b>UwUstimates</b> is a planning poker tool designed to help development teams. It's an easy-to-use
-                    tool,
-                    for you and your team to estimate and manage tasks in the Agile methodology, in a kawaii matter (´・ω・｀)
-                </p>
-                <div class='xs:mt-1 lg:mt-5 self-start'>
-                    <ul class='font-secondary list-outside list-disc ml-10 font-light xs:text-sm lg:text-lg'>
-                        <li class='xs:p-1 lg:p-2'>Estimate your tasks</li>
-                        <li class='xs:p-1 lg:p-2'>Roleplay as one of many anime avatars</li>
-                        <li class='xs:p-1 lg:p-2'>Get paid doing it!</li>
-                    </ul>
-                </div>
+    <article class='mx-5'>
+        <div class='flex flex-col justify-start items-center gap-5'>
+            <h2 class='xs:text-lg lg:text-2xl'>Your perfect planning tool</h2>
+            <p class='font-secondary xs:text-sm lg:text-md font-light text-center'>
+                <b>UwUstimates</b> is a planning poker tool designed to help development teams. It's an easy-to-use
+                tool,
+                for you and your team to estimate and manage tasks in the Agile methodology, in a kawaii matter <span
+                    class='whitespace-nowrap'>(´・ω・｀)</span>
+            </p>
+            <div class='xs:mt-1 lg:mt-5 self-start'>
+                <ul class='font-secondary list-outside list-none ml-10 font-light xs:text-sm lg:text-lg'>
+                    <li class='xs:p-1 lg:p-2 flex justify-start items-center gap-3'>
+                        <img src="../assets/rocket.svg" alt="" class='aspect-square xs:w-4 lg:w-6'>
+                        Estimate your tasks
+                    </li>
+                    <li class='xs:p-1 lg:p-2 flex justify-start items-center gap-3'>
+                        <img src="../assets/rocket.svg" alt="" class='aspect-square xs:w-4 lg:w-6'>
+                        Roleplay as one of many anime avatars
+                    </li>
+                    <li class='xs:p-1 lg:p-2 flex justify-start items-center gap-3'>
+                        <img src="../assets/rocket.svg" alt="" class='aspect-square xs:w-4 lg:w-6'>
+                        Get paid doing it!
+                    </li>
+                </ul>
             </div>
-        </article>
+        </div>
+    </article>
 </template>
 <script>
 export default {
