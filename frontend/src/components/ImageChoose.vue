11b3971a248086cc806b9783caf95e91b38d354b
Kamil <kjas92@gmail.com>
2023-05-11 10:10:00 +0200

    fix size of image list

--- frontend/src/components/ImageChoose.vue
+++ frontend/src/components/ImageChoose.vue
@@ -1,13 +1,12 @@
 <template>
     <div class='flex items-center justify-center bg-none'>
         <p class='text-left font-light xs:text-xs lg:text-lg'>{{ label }} : </p>
-
         <div class='flex justify-center w-3/5 align-middle cursor-pointer relative' v-on:click="toggle">
             <div class='rounded-full aspect-square xs:w-12 sm:w-14 2xl:w-20 overflow-hidden hover:shadow-bg3 hover: shadow-md'>
                 <img v-show="!isOpen" class='scale-100 hover:scale-110 ' :src="getIconUrl(value)" alt="">
             </div>
 
-            <div v-show="isOpen" class='flex flex-wrap items-center justify-center gap-1 py-1 flex-shrink xs:w-[50vw] sm:min-w-[50vw] 2xl:min-w-[45vw] bg-bg2 rounded-xl bg-opacity-70 absolute'
+            <div v-show="isOpen" class='flex flex-wrap items-center justify-center gap-1 xs:py-0 lg:py-1 flex-shrink xs:w-[90vw] lg:w-[40vw] bg-bg2 rounded-xl bg-opacity-70 absolute xs:-right-12'
                 v-on:mouseleave="hide">
                 <div v-for=" img, index in images()"
                     class='rounded-full aspect-square sm::min-w-[1.5rem] xs:w-10 sm:max-w-[3rem] 2xl:min-w-[4rem] 2xl:max-w-[5rem] overflow-hidden hover: shadow-md hover:shadow-bg3 focus:scale-xl'
