7a2220272eadf70a25e793d68789374582625ade
Kamil <kjas92@gmail.com>
2023-06-14 20:37:15 +0200

    fix bgs

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -43,8 +43,8 @@ module.exports = {
         'revers': "url('/src/assets/revers.png')",
         'front': "url('/src/assets/front.png')",
         'background': "url('/src/assets/background.jpeg')",
-        'background-second': "url('/src/assets/background.png')",
-        'background-third': "url('/src/assets/background2.png')",
+        'background-second': "url('/src/assets/background.jpeg')",
+        'background-third': "url('/src/assets/background.jpeg')",
       },
       backgroundSize: {
         'small': '90%'
