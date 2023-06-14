b1da51be123d134abdc9028e461ddc68f3b8e4bf
Kamil <kjas92@gmail.com>
2023-06-14 21:59:48 +0200

    fix checkbox

--- frontend/src/components/Checkbox.vue
+++ frontend/src/components/Checkbox.vue
@@ -1,6 +1,6 @@
 <template>
     <div class='flex justify-center cursor-pointer hover:shadow-lg active:shadow-xl' v-on:click="select">
-        <div class='w-3 h-3 rounded-md border border-white-500 flex items-center justify-center'>
+        <div class='w-3 h-3 pl-1 rounded-md border border-white-500 flex items-center justify-center'>
             <p v-show="selected" class='text-center text-main-font'>✓</p>
         </div>
         <p class='ml-2 font-secondary text-main-font text-[12px] font-normal cursor-pointer '>{{ label }}</p>
