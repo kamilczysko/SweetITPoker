b5bdd0eca10cbfe621e54e3d0c63db5cfe37832a
Kamil <kjas92@gmail.com>
2023-07-14 12:49:42 +0200

    fix scroll again

--- frontend/src/components/game/GameTable.vue
+++ frontend/src/components/game/GameTable.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="w-full flex justify-center items-center xs:gap-2 md:gap-5 flex-wrap m-auto p-5 rounded-r-lg overflow-y-scroll h-full">
+    <div class="w-full flex justify-center items-center xs:gap-2 md:gap-5 flex-wrap m-auto p-5 rounded-r-lg scrollbar-hide overflow-y-scroll h-full">
         <ReversCard v-if="this.$store.state.result == null" v-for="player in nonObservers" :key="player.id" :data="player"></ReversCard>
         <FrontCard v-else v-for="player in nonObservers" :unit="player.selectedUnit" :value="player.selectedCard"></FrontCard>
     </div>
