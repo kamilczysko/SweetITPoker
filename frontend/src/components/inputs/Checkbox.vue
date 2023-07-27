e184c3f79b34dd8df576059fbff8871ea5719eb0
Kamil <kjas92@gmail.com>
2023-07-27 20:25:35 +0200

    fix text size

--- frontend/src/components/inputs/Checkbox.vue
+++ frontend/src/components/inputs/Checkbox.vue
@@ -1,6 +1,6 @@
 <template>
     <div class='flex justify-center items-center gap-3 font-main font-extralight cursor-pointer h-fit' v-on:click="select">
-        <p class='ml-2 text-[12px] font-normal cursor-pointer '>{{ label }}</p>
+        <p class='ml-2 text-md font-normal cursor-pointer '>{{ label }}</p>
         <div class='w-3 h-3 pb-1 pl-1 rounded-md border border-black flex items-center justify-center hover:scale-110 active:scale-125 select-none'>
             <p v-show="selection" class='text-center font-extrabold text-xl'>✓</p>
         </div>
